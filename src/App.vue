4c5c29f3ca5e8c916247b89b3782d977b49168ae
XZW <1246018053@qq.com>
2022-10-03 14:59:25 +0800

    first commit

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<template>
+  <header class="header">
+    <NavBar></NavBar>
+  </header>
+  <router-view></router-view>
+</template>
+
+<script setup>
+import "./assets/base.css";
+import TheIcon from "./components/TheIcon.vue";
+import NavBar from "./components/NavBar.vue";
+</script>
+
+<style scoped>
+.header {
+  height: 80px;
+  box-shadow: 0px 4px 24px rgb(0, 0, 0, 0.08);
+}
+</style>
---
+++ src/components/PostItem.vue
@@ -0,0 +1,73 @@
+<template>
+  <div class="postItem">
+    <img alt="" width="100%" height="100%" style="background: #eee" />
+    <div class="postInfo">
+      <div class="postMeta">
+        <TheAvatar />
+        <span>11111</span>
+        <span class="postPubDate">11111</span>
+        <PostActions />
+      </div>
+      <div class="postDesc">
+        <p>11112222</p>
+      </div>
+    </div>
+  </div>
+</template>
+<script setup>
+import TheAvatar from "../components/TheAvatar.vue";
+import PostActions from "../components/PostActions.vue";
+
+defineProps({
+  post: {
+    type: Object,
+    default: {},
+  },
+});
+</script>
+<style scoped>
+.postItem {
+  box-shadow: 0px 12px 24px rgba(0, 0, 0, 0.08);
+  border-radius: 8px;
+}
+
+.postInfo {
+  padding: 24px;
+}
+
+.postItem > img {
+  width: 100%;
+  height: 400px;
+  object-fit: cover;
+  background: #eee;
+  cursor: pointer;
+}
+
+.postMeta {
+  display: grid;
+  grid-template-areas:
+    "avatar name actions"
+    "pubDate pubDate actions";
+  grid-template-columns: 42px 1fr 3fr;
+  row-gap: 6px;
+}
+.postMeta .avatar {
+  grid-area: avatar;
+}
+
+.postMeta .postPubDate {
+  grid-area: pubDate;
+  color: #9f9f9f;
+  font-size: 14px;
+}
+
+.postActions {
+  grid-area: actions;
+  justify-self: end;
+}
+
+.postDesc {
+  margin-top: 28px;
+  white-space: pre-line;
+}
+</style>
